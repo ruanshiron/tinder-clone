f41b2e6ab0df64d6de586e45639888a45d6679d6
Vinh <theten12@gmail.com>
2019-12-21 15:02:20 +0700

    initial with rails-react

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const App = () => (
+  <div>App Component </div>
+)
+
+App.defaultProps = {
+}
+
+App.propTypes = {
+}
+
+export default App
